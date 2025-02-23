4c4799838089cc1488c4bdb57180c880edf5338d
AdamFrye <amfrye7@gmail.com>
2025-02-22 21:30:37 -0600

    adds the ability to tie, vertical and horizontal hash marks, ability to change board size, and error handling

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,28 @@ public class Main {
         clearConsole();
         Scanner console = new Scanner(System.in);
 
+        //getting valid board size.
+        System.out.println("Enter desired board size.");
+        boolean validInput = false;
+        int boardSize = 1;
+        //error handling for boneheads.
+        while(!validInput) {
+            try {
+                boardSize = console.nextInt();
+                validInput = true;
+            } catch (InputMismatchException err){
+                clearConsole();
+                System.out.println("Enter a VALID board size.");
+                console.next();
+            }
+        }
+
         //in this board, 0 will be null space, 1 will be X, 2 will be O.
-        //int[][] board = new int[3][3];
-        int[][] board = {{1,1,1},{1,1,1},{1,1,1}};
+        int[][] board = new int[boardSize][boardSize];
 
         //if turn = 1, X will play, if turn = 2, O will play.
         int userTurn = 1;
+        int turnCount = 1;
         boolean gameOver = false;
         boolean invalidLocation = false;
         while(!gameOver){
@@ -35,21 +51,30 @@ public class Main {
                 continue;
             }
             
-            //making sure the moves do not overlap.
-            if (selRow > 3 || selRow < 0 || selCol > 3 || selRow < 0 || board[selRow][selCol] > 0) {
+            //making sure the moves are within bounds as well as not overlapping.
+            try {
+                if (board[selRow][selCol] > 0){
+                    invalidLocation = true;
+                    continue;
+                }
+            } catch (ArrayIndexOutOfBoundsException err){
                 invalidLocation = true;
                 continue;
             }
             board[selRow][selCol] = userTurn;
             
             //checking win condition and printing win screen
-            if (checkWinner(board, userTurn)){
+            if (checkWinner(board, userTurn) || turnCount == Math.pow(board.length, 2)){
                 clearConsole();
                 printBoard(board);
-                System.out.printf("Player %d Wins !!%n", userTurn);  
+                if (turnCount == Math.pow(board.length, 2))
+                    System.out.print("Tie game. Player 1 and Player 2 lose.");
+                else 
+                    System.out.printf("Player %d Wins !!%n", userTurn);  
                 gameOver = true;
             }
             
+            turnCount++;
             //logic for switching turns.
             switch (userTurn) {
                 case 1: 
@@ -78,12 +103,17 @@ public class Main {
                     else if (board[row][j] == 1) System.out.print(" X ");
                     else System.out.print(" O ");
 
+                    //for printing the vertical hashes.
                     if (j != board.length - 1) System.out.print("|");
                 }
+                row++;
             }
-            else {
-                for (int j = 0; j < (3*board.length) + (board.length - 1); j++) System.out.print("-");
-            }
+            else
+                //for printing the horizontal hashes.
+                for (int j = 1; j < (3*board.length) + (board.length); j++) {
+                    if (!(j % 4 == 0)) System.out.print("-");
+                    else System.out.print("+");
+                }
             System.out.println();
         }
     }
@@ -91,28 +121,28 @@ public class Main {
     //long way of checking for winner. Could be done with recursion but idk which is faster.
     public static boolean checkWinner(int[][] board, int target){
         //checks rows for X win.
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board.length; j++){
                 if (board[i][j] != target) break;
-                else if (j == 2) return true;
+                else if (j == board.length-1) return true;
             }
         }
         //check columns for win.
-        for (int j = 0; j < 3; j++){
-            for (int i = 0; i < 3; i++){
+        for (int j = 0; j < board.length; j++){
+            for (int i = 0; i < board.length; i++){
                 if (board[i][j] != target) break;
-                else if (i == 2) return true;
+                else if (i == board.length-1) return true;
             }
         }
         //check left diagonal for win.
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < board.length; i++){
             if (board[i][i] != target) break;
-            else if (i == 2) return true;
+            else if (i == board.length-1) return true;
         }
         //check right diagonal for win.
-        for (int i = 0, j = 2; i < 3; i++, j--){
+        for (int i = 0, j = board.length-1; i < board.length; i++, j--){
             if (board[i][j] != target) break;
-            else if (i == 2) return true;
+            else if (i == board.length-1) return true;
         }
 
         return false;
