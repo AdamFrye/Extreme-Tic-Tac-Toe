bfe90466956670a60be319a241754e4001b0c160
AdamFrye <amfrye7@gmail.com>
2025-02-24 11:44:45 -0600

    removes like one redundancy. this is the final build.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
-//TODO: add function to quit.
 public class Main {
     public static void main(String[] args) throws Exception {
-        clearConsole();
         Scanner console = new Scanner(System.in);
 
         //Prompt for Single Player or Multiplayer.
